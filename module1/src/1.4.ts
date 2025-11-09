025e4962e45ef7d7d0a99134f767347e4a175e9e
Rudra Protap Chakraborty <rudra.phymos@gmail.com>
2025-11-09 10:01:30 +0530

    Basic data types

---
+++ module1/src/1.4.ts
@@ -0,0 +1,36 @@
+//Basic Data Type
+
+//string
+let firstName:string = 'rudra'
+
+//number
+let roll:number = 123
+
+//boolean
+let isAdmin: boolean = true
+
+//undefined
+let x: undefined = undefined
+
+//null
+let y: null = null
+
+let d: number;
+
+d=123
+
+//array
+
+let friends: string[] = ['rachel', 'monica']
+
+let eligibleRollList: number[] = [1, 3]
+
+eligibleRollList.push('dd')
+
+//tuple --> array --> order --> type of values
+
+let coordinates: [number, number] = [1,3]
+
+let ageName: [number, string, boolean] = [50, 'rudra', true]
+
+ageName[0] = 67
