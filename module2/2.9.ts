e5d2109454a633c3b8ebb968377d253785b836bd
Rudra Protap Chakraborty <rudra.phymos@gmail.com>
2025-11-09 17:54:52 +0530

    Conditional types

---
+++ module2/2.9.ts
@@ -0,0 +1,59 @@
+{
+
+//conditional type
+
+type a1 = number
+type b1 = string
+
+type x = a1 extends null ? true : false // conditional type
+
+type y = a1 extends null ? true : b1 extends undefined ? undefined : any;
+
+type Sheikh = {
+    bike: string;
+    car: string;
+    ship: string;
+}
+
+type CheckVehicle<T> = T extends keyof Sheikh ? true : false
+
+type HasBike = CheckVehicle<"car">
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
