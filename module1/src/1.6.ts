163bb619f0cfdffac43e69ff27325e8373f29fcc
Rudra Protap Chakraborty <rudra.phymos@gmail.com>
2025-11-09 10:21:31 +0530

    Function in typescript

---
+++ module1/src/1.6.ts
@@ -0,0 +1,25 @@
+//learning functions
+//normal function
+//arrow function
+
+function add(num1: number, num2:number): number{
+    return num1+num2;
+}
+
+add(2,3)
+
+const addArrow = (num1:number, num2:number): number => num1+num2
+
+//object --> function --> method
+
+const poorUser = {
+    name: 'rudra',
+    balance: 0,
+    addBalance(balance: number): string{
+        return `My new balance is: ${this.balance+balance}`;
+    }
+}
+
+const arr: number [] = [1,2,3]
+
+const newArray: number[] = arr.map((elem: number): number=>elem*elem)
