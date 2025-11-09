2c3792952e208bddd8a63dee7c4d9f30c2ff3a08
Rudra Protap Chakraborty <rudra.phymos@gmail.com>
2025-11-09 10:11:56 +0530

    Object , Optional and Literal Types

---
+++ module1/src/1.5.ts
@@ -0,0 +1,15 @@
+//Reference Type --> Object
+
+const user: {
+    company: 'Programming Hero'; //type --> literal type
+    firstName: string;
+    middleName?: string; //optional type
+    lastName: string;
+    isMarried: boolean;
+} = {
+    company: 'Programming Hero',
+    firstName: 'rudra',
+    middleName: 'protap',
+    lastName: 'chakraborty', 
+    isMarried: false,
+}
