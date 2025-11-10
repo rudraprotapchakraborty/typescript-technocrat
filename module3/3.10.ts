ea8b68b1456d7c9a7403d15509cf40e5328b88a1
Rudra Protap Chakraborty <rudra.phymos@gmail.com>
2025-11-11 00:15:34 +0530

    Encapsulation in OOP

---
+++ module3/3.10.ts
@@ -0,0 +1,80 @@
+{
+
+//access modifiers
+class BankAccount{
+    public readonly id: number;
+    name: string;
+    // private _balance: number;
+    protected _balance: number;
+
+    constructor(id: number, name: string, balance: number){
+        this.id =id;
+        this.name=name;
+        this._balance = balance;
+    }
+    public addDeposit(amount: number){
+        this._balance = this._balance+amount
+    }
+    public getBalance(){
+        return this._balance;
+    }
+
+    getHiddenMethod(){
+        return this.getBalance()
+    }
+}
+
+class StudentAccount extends BankAccount{
+    test(){
+        this.getHiddenMethod
+    }
+}
+
+const goribManusherAccount = new BankAccount(111, "Mr. gorib", 20);
+
+goribManusherAccount.addDeposit(300)
+const myBalance = goribManusherAccount.getBalance()
+console.log(myBalance)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
